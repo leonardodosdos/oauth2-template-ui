f8fa131dbe268d8093529dd6b10994f07cb99365
Leonardo Dosdos III <leonardodosdos@gmail.com>
2023-12-02 23:13:34 +0800

    Added react-router

---
+++ src/components/Layout/PrivateLayout.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+
+interface PrivateLayoutProps { }
+
+const PrivateLayout: FC<PrivateLayoutProps> = () => (
+    <div>
+        PrivateLayout Compoenent
+    </div>
+)
+
+export default PrivateLayout
---
+++ src/components/Layout/PublicLayout.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+
+interface PublicLayoutProps { }
+
+const PublicLayout: FC<PublicLayoutProps> = () => (
+  <div>
+    PublicLayout Component
+  </div>
+)
+
+export default PublicLayout
